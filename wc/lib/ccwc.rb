1bd7dad0183cfdeea8ee350c71ec03caabfa0ddc
Rahmane Ousmane <ousmanedev@gmail.com>
2024-10-27 02:30:02 +0200

    Reduce complexity to O(N) for computing all the stats

--- wc/lib/ccwc.rb
+++ wc/lib/ccwc.rb
@@ -1,30 +1,31 @@
 class Ccwc
-  def initialize(file)
-    @lines = file.readlines
-  end
-
-  def line_count
-    @lines.size
-  end
-
-  def byte_count
-    @lines.inject(0) { |sum, line| sum + line.bytesize }
-  end
-
-  def word_count
-    @lines.inject(0) { |sum, line| sum + line.split.size }
-  end
-
-  def character_count
-    @lines.inject(0) { |sum, line| sum + line.length }
-  end
-
-  def summary
-    [line_count, word_count, byte_count]
-  end
+  attr_reader :line_count, :word_count, :byte_count, :character_count
 
   alias :l :line_count
   alias :w :word_count
   alias :c :byte_count
   alias :m :character_count
+
+  def initialize(file)
+    @file = file
+    @line_count = 0
+    @word_count = 0
+    @byte_count = 0
+    @character_count = 0
+
+    build_stats
+  end
+
+  def build_stats
+    @file.readlines.each do |line|
+      @line_count += 1
+      @byte_count += line.bytesize
+      @word_count += line.split.size
+      @character_count += line.length
+    end
+  end
+
+  def summary
+    [line_count, word_count, byte_count]
+  end
 end
